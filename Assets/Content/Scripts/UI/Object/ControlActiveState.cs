b31c3969fadbe331764b3f10e7a050f974a21e5f
MichaelBitsakis <infogdrs@yahoo.com>
2023-12-04 13:27:19 +0200

    Multi UI

--- Assets/Content/Scripts/UI/Object/ControlActiveState.cs
+++ Assets/Content/Scripts/UI/Object/ControlActiveState.cs
@@ -3,31 +3,26 @@ using System.Collections.Generic;
 
 public class ControlActiveState : MonoBehaviour
 {
-    public List<GameObject> items; // List of items to control
+    public List<GameObject> openItems; // List of items to activate
+    public List<GameObject> closeItems; // List of items to deactivate
     public bool close = false;
     public bool open = false;
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public void Control()
     {
         if (close)
         {
-            SetActiveStateOfItems(false);
-            close = false; // Reset the flag
+            SetActiveStateOfItems(closeItems, false);
+            //close = false; // Reset the flag
         }
-        else if (open)
+        if (open)
         {
-            SetActiveStateOfItems(true);
-            open = false; // Reset the flag
+            SetActiveStateOfItems(openItems, true);
+            //open = false; // Reset the flag
         }
     }
 
-    private void SetActiveStateOfItems(bool state)
+    private void SetActiveStateOfItems(List<GameObject> items, bool state)
     {
         foreach (var item in items)
         {
