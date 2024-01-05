1d218aa64b9da891f7e9fb4ec2749a6ae841f71e
MichaelBitsakis <infogdrs@yahoo.com>
2024-01-05 11:24:27 +0200

    UI Linker follows PlutoPhotoSphere

--- Assets/Content/Scripts/PhotoSphere/SphereColliderControl.cs
+++ Assets/Content/Scripts/PhotoSphere/SphereColliderControl.cs
@@ -3,25 +3,34 @@ using UnityEngine;
 public class SphereColliderControl : MonoBehaviour
 {
     private Collider sphereCollider;
+    private Collider plutoSphereCollider;
     public GameObject InteractionAffordance;
+    public GameObject PlutoSphere;
 
     void Start()
     {
         sphereCollider = GetComponent<Collider>();
+        plutoSphereCollider = GetComponent<MeshCollider>();
     }
 
     // Call this method to disable the collider
     public void DisableCollider()
     {
         sphereCollider.enabled = false;
+        plutoSphereCollider.enabled = false;
+        
         InteractionAffordance.transform.gameObject.SetActive(false);
+        PlutoSphere.transform.gameObject.SetActive(false);
     }
 
     // Call this method to enable the collider
     public void EnableCollider()
     {
         sphereCollider.enabled = true;
+        plutoSphereCollider.enabled = true;
+
         InteractionAffordance.transform.gameObject.SetActive(true);
+        PlutoSphere.transform.gameObject.SetActive(true);
 
     }
 }
